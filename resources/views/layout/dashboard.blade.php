a5d84864648d6cb5f6f2a4fb9cf72de7b97f94a4
Giovanni D'Apote <ironman@iMac-Pro-di-Giovanni.local>
2020-03-18 11:09:34 +0100

    Layout Base and Table migration

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -1,48 +1,8 @@
+@extends('layout.template')
 
-<!DOCTYPE html>
-<html lang="en">
+@section('title', 'Dashboard')
 
-<head>
+@section('content')
+    <h2>Dasboard</h2>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Simple Sidebar - Start Bootstrap Template</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('css/simple-sidebar.css') }}" rel="stylesheet">
-
-</head>
-
-<body>
-
-<div class="d-flex" id="wrapper">
-
-    <!-- Sidebar -->
-    @include('layout.sidebar')
-    <!-- /#sidebar-wrapper -->
-
-    <!-- Page Content -->
-   @include('layout.navbar')
-    <!-- /#page-content-wrapper -->
-
-</div>
-
-<!-- Bootstrap core JavaScript -->
-<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
-
-<!-- Custom JS for dashboard -->
-<script src="{{ asset('js/dashboard.js') }}"></script>
-
-@yield('scripts')
-
-</body>
-
-</html>
-
+@stop
